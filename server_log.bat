51b3205581490e51e0ff2f8ddc020a95303d416a
Riccardo Sagramoni <50587373+RiccardoSagramoni@users.noreply.github.com>
2020-07-03 15:56:29 +0200

    Delete server_log.bat

--- server_log.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set L=c:\prg\libs\
-set LIBS=%L%xstream-1.4.7.jar;%L%xmlpull-1.1.3.1.jar;%L%xpp3_min-1.1.4c.jar;
-set CLASSES=.\build\classes\
-
-c:\prg\jdk8\bin\java -classpath %LIBS%%CLASSES% ServerLogEventiGUI
-pause
